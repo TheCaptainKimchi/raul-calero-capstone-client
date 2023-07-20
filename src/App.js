abc87a872c807ae423c029221151202b5eea1c56
Raul Calero <raulcalero7@gmail.com>
2023-07-19 17:29:27 -0700

    Header component completed adding in background color black, app logo, and burger menu linking to home, about, and feature page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" />
           <Route path="/about" />
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import "./Header.scss";
+import Logo from "../../Assets/Images/SparkLogo.png";
+import {elastic as Menu } from "react-burger-menu";
+import {Link} from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      
+      <img className="header__logo" src={Logo} alt="spark-logo"></img>
+      <Menu right>
+        <Link id="home" className="menu-item" to={"/"}>Home</Link>
+        <Link id="home" className="menu-item" to={"/"}>Feature</Link>
+        <Link id="home" className="menu-item" to={"/"}>About</Link>
+      </Menu>
     </div>
   )
 }
