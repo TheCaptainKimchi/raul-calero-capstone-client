4348e00d50bd95309652cbc6ab9772b71d4dccc9
Raul Calero <raulcalero7@gmail.com>
2023-07-27 22:05:00 -0700

    Added responsive design to home, feature, and results pages.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,40 @@
 import "./Header.scss";
 import Logo from "../../Assets/Images/SparkLogo.png";
-import {elastic as Menu } from "react-burger-menu";
-import {Link} from "react-router-dom";
+import { elastic as Menu } from "react-burger-menu";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to={"/"} className="header__logo"><img src={Logo} alt="spark-logo"></img></Link>
-      <Menu right>
-        <Link id="home" className="menu-item" to={"/"}>Home</Link>
-        <Link id="home" className="menu-item" to={"/"}>Feature</Link>
-        <Link id="home" className="menu-item" to={"/"}>About</Link>
+      <Link to={"/"} className="header__logo">
+        <img src={Logo} alt="spark-logo"></img>
+      </Link>
+      <Menu right className="burger-menu">
+        <Link id="home" className="menu-item" to={"/"}>
+          Home
+        </Link>
+        <Link id="home" className="menu-item" to={"/"}>
+          Feature
+        </Link>
+        <Link id="home" className="menu-item" to={"/"}>
+          About
+        </Link>
       </Menu>
+      <nav className="header__nav">
+        <ul className="header__nav-list">
+          <li className="header__nav-list-item">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="header__nav-list-item">
+            <Link to={"/feature"}>Feature</Link>
+          </li>
+          <li className="header__nav-list-item">
+            <Link to={"/about"}>About</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
