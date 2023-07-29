9c52f1295d27a5eff5930bb0067398e50aad6141
Raul Calero <raulcalero7@gmail.com>
2023-07-29 10:21:02 -0700

    Started styling for profile page displaying match history and soon working in overall match history statistics.

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import maps from "../../Data/Maps.json";
 import agents from "../../Data/Agents.json";
+import "./Profile.scss";
 
 const Profile = ({ isLoggedIn, setIsLoggedIn }) => {
   const [profileData, setProfileData] = useState(null);
@@ -24,37 +25,48 @@ const Profile = ({ isLoggedIn, setIsLoggedIn }) => {
 
     try {
       // Do a GET request to the /profile endpoint to get the user's data
-      const response1 = await axios.get(`http://localhost:8080/profile`, {
-        headers: {
-          authorization: `Bearer ${authToken}`,
-        },
-      });
+      await axios
+        .get(`http://localhost:8080/profile`, {
+          headers: {
+            authorization: `Bearer ${authToken}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          // Update the profileData state with the received data
+          setProfileData(response.data);
 
-      // Update the profileData state with the received data
-      setProfileData(response1.data);
+          const fetchData = async () => {
+            // Now that profileData is updated, we can make the second Axios request
+            const response2 = await axios.get(
+              `http://localhost:8080/matchId?puuid=${response.data.token.puuid}`
+            );
 
-      // Now that profileData is updated, we can make the second Axios request
-      const response2 = await axios.get(
-        `http://localhost:8080/matchId?puuid=${response1.data.token.puuid}`
-      );
+            // Sort through response2 and store matchIds in a list
+            response2.data.map((matchId) => {
+              return matchList.push(matchId.matchId);
+            });
+            // Iterate through matchList to make calls to obtain each match data from array
+            for (let i = 0; i < 9 && i < matchList.length; i++) {
+              const response3 = await axios.get(
+                `http://localhost:8080/match?matchId=${matchList[i]}`
+              );
+              matchDataList.push(response3.data);
+            }
 
-      // Sort through response2 and store matchIds in a list
-      response2.data.map((matchId) => {
-        return matchList.push(matchId.matchId);
-      });
-
-      // Iterate through matchList to make calls to obtain each match data from array
-      for (let i = 0; i < 9 && i < matchList.length; i++) {
-        const response3 = await axios.get(
-          `http://localhost:8080/match?matchId=${matchList[i]}`
-        );
-        matchDataList.push(response3.data);
-      }
-
-      setMatchData(matchDataList);
+            setMatchData(matchDataList);
+          };
+          fetchData();
+        });
     } catch (error) {
       setMatchData("Error");
       console.error("Error fetching data:", error);
+      // Unset the local storage auth token
+      localStorage.removeItem("authToken");
+
+      // Set the state to logged out
+      setIsLoggedIn(false);
+      setProfileData(null);
     }
   };
 
@@ -76,124 +88,138 @@ const Profile = ({ isLoggedIn, setIsLoggedIn }) => {
   }
 
   return (
-    <section>
-      <h2>
-        Welcome {profileData.token.riotId} #{profileData.token.tagline}
-      </h2>
-      <button onClick={handleLogout}>Logout</button>
-      {matchData.map((match) => {
-        const key = match.matchInfo.matchId;
+    <div className="profile">
+      <div className="profile__top">
+        <h2>
+          Welcome {profileData.token.riotId} #{profileData.token.tagline}
+        </h2>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <div className="profile__bot">
+        <div className="profile__bot-left">
+          <p>Test</p>
+        </div>
+        <div className="profile__bot-right">
+          {matchData.map((match) => {
+            const key = match.matchInfo.matchId;
 
-        if (renderedKeys.has(key)) {
-          return null;
-        }
-
-        const playerDetails = match.players.find((player) => {
-          return (
-            player.gameName.localeCompare(profileData.token.riotId, undefined, {
-              sensitivity: "base",
-            }) === 0
-          );
-        });
-
-        console.log(`===== ${playerDetails.gameName} =====`);
-
-        const kda =
-          (playerDetails.stats.kills + playerDetails.stats.assists) /
-          playerDetails.stats.deaths;
-
-        const acs = playerDetails.stats.score / match.roundResults.length - 1;
-
-        const map = maps.find(
-          (map) => map.assetPath === match.matchInfo.mapId
-        )?.name;
-
-        const agent = agents.find((agent) => {
-          const agentSearch = agent.id.toLowerCase();
-          const agentPlayed = playerDetails.characterId.toLowerCase();
-
-          return agentSearch === agentPlayed;
-        });
-
-        let mode = "";
-        if (match.matchInfo.queueId === "ggteam") {
-          mode = "escalation";
-        } else if (match.matchInfo.queueId === "hurm") {
-          mode = "team deathmatch";
-        } else if (!match.matchInfo.queueId) {
-          mode = "custom";
-        } else {
-          mode = match.matchInfo.queueId;
-        }
-
-        renderedKeys.add(key);
-
-        const matchInfo = {
-          id: key,
-          userName: playerDetails.gameName,
-          tagline: playerDetails.tagLine,
-          puuid: playerDetails.puuid,
-          kills: playerDetails.stats.kills,
-          deaths: playerDetails.stats.deaths,
-          assists: playerDetails.stats.assists,
-          kda: kda,
-          acs: acs,
-          map: map,
-          agent: agent.name,
-          mode: mode,
-        };
-
-        async function postData() {
-          try {
-            const response = await axios.post(
-              `http://localhost:8080/leaderboard?id=${matchInfo.id}&userName=${matchInfo.userName}&tagline=${matchInfo.tagline}&puuid=${matchInfo.puuid}&kills=${matchInfo.kills}&deaths=${matchInfo.deaths}&assists=${matchInfo.assists}&kda=${matchInfo.kda}&acs=${matchInfo.acs}&map=${matchInfo.map}&agent=${matchInfo.agent}&mode=${matchInfo.mode}`
-            );
-            console.log("Response data:", response.data);
-
-            return response.data;
-          } catch (error) {
-            if (
-              error.response.data.error !=
-              "Data with the same id already exists"
-            ) {
-              console.error("Error posting data:", error);
+            if (renderedKeys.has(key)) {
+              return null;
             }
-          }
-        }
-        postData();
 
-        return (
-          <div className={`match-container ${map}`} key={key}>
-            <div className={`match-container__header`}>
-              <h3 className="match-container__header-mode">{mode}</h3>
-              <h3 className="match-container__header-map">{map}</h3>
-            </div>
-            <table className="match-container__table">
-              <thead>
-                <tr>
-                  <th>Agent</th>
-                  <th>Kills</th>
-                  <th>Deaths</th>
-                  <th>Assists</th>
-                  <th>KDA</th>
-                  <th>ACS</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th>{agent.name}</th>
-                  <th>{playerDetails.stats.kills}</th>
-                  <th>{playerDetails.stats.deaths}</th>
-                  <th>{playerDetails.stats.assists}</th>
-                  <th>{Math.round(kda * 10) / 10}</th>
-                  <th>{Math.round(acs * 10) / 10}</th>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
-    </section>
+            const playerDetails = match.players.find((player) => {
+              return (
+                player.gameName.localeCompare(
+                  profileData.token.riotId,
+                  undefined,
+                  {
+                    sensitivity: "base",
+                  }
+                ) === 0
+              );
+            });
+
+            console.log(`===== ${playerDetails.gameName} =====`);
+
+            const kda =
+              (playerDetails.stats.kills + playerDetails.stats.assists) /
+              playerDetails.stats.deaths;
+
+            const acs =
+              playerDetails.stats.score / match.roundResults.length - 1;
+
+            const map = maps.find(
+              (map) => map.assetPath === match.matchInfo.mapId
+            )?.name;
+
+            const agent = agents.find((agent) => {
+              const agentSearch = agent.id.toLowerCase();
+              const agentPlayed = playerDetails.characterId.toLowerCase();
+
+              return agentSearch === agentPlayed;
+            });
+
+            let mode = "";
+            if (match.matchInfo.queueId === "ggteam") {
+              mode = "escalation";
+            } else if (match.matchInfo.queueId === "hurm") {
+              mode = "team deathmatch";
+            } else if (!match.matchInfo.queueId) {
+              mode = "custom";
+            } else {
+              mode = match.matchInfo.queueId;
+            }
+
+            renderedKeys.add(key);
+
+            const matchInfo = {
+              id: key,
+              userName: playerDetails.gameName,
+              tagline: playerDetails.tagLine,
+              puuid: playerDetails.puuid,
+              kills: playerDetails.stats.kills,
+              deaths: playerDetails.stats.deaths,
+              assists: playerDetails.stats.assists,
+              kda: kda,
+              acs: acs,
+              map: map,
+              agent: agent.name,
+              mode: mode,
+            };
+
+            async function postData() {
+              try {
+                const response = await axios.post(
+                  `http://localhost:8080/leaderboard?id=${matchInfo.id}&userName=${matchInfo.userName}&tagline=${matchInfo.tagline}&puuid=${matchInfo.puuid}&kills=${matchInfo.kills}&deaths=${matchInfo.deaths}&assists=${matchInfo.assists}&kda=${matchInfo.kda}&acs=${matchInfo.acs}&map=${matchInfo.map}&agent=${matchInfo.agent}&mode=${matchInfo.mode}`
+                );
+                console.log("Response data:", response.data);
+
+                return response.data;
+              } catch (error) {
+                if (
+                  error.response.data.error !=
+                  "Data with the same id already exists"
+                ) {
+                  console.error("Error posting data:", error);
+                }
+              }
+            }
+            postData();
+
+            return (
+              <div className={`match-container ${map}`} key={key}>
+                <div className={`match-container__header`}>
+                  <h3 className="match-container__header-mode">{mode}</h3>
+                  <h3 className="match-container__header-map">{map}</h3>
+                </div>
+                <table className="match-container__table">
+                  <thead>
+                    <tr>
+                      <th>Agent</th>
+                      <th>Kills</th>
+                      <th>Deaths</th>
+                      <th>Assists</th>
+                      <th>KDA</th>
+                      <th>ACS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th>{agent.name}</th>
+                      <th>{playerDetails.stats.kills}</th>
+                      <th>{playerDetails.stats.deaths}</th>
+                      <th>{playerDetails.stats.assists}</th>
+                      <th>{Math.round(kda * 10) / 10}</th>
+                      <th>{Math.round(acs * 10) / 10}</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
 
