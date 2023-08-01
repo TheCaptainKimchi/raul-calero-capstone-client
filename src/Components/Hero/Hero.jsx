a824ae6778e3518e3f1c8b8b8bebfe3418c31403
Raul Calero <raulcalero7@gmail.com>
2023-08-01 16:33:16 -0700

    Restructred home page styling and layout

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,14 +6,19 @@ const Hero = () => {
   // Render hero component
   return (
     <div className="hero">
-      <h1 className="hero__title">SparkGG</h1>
-      <p className="hero__text">
-        Current of Champions: Take your Valorant skills to the next level with
-        our data analytics!
-      </p>
-      <Link className="hero__button" to={"/feature"}>
-        <button>START NOW</button>
-      </Link>
+      <video autoPlay loop muted>
+        <source src="https://capstoneserver-9d48f9576e5c.herokuapp.com/videos/valorant.mp4"></source>
+      </video>
+      <div className="hero__container">
+        <h1 className="hero__title">SparkGG</h1>
+        <p className="hero__text">
+          Current of Champions: Take your Valorant skills to the next level with
+          our data analytics!
+        </p>
+        <Link className="hero__button" to={"/feature"}>
+          <button>START NOW</button>
+        </Link>
+      </div>
     </div>
   );
 };
